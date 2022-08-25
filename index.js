822629e38df726d813867adce2987025c6c67055
Ruth Jepkalal <kipchauijepkaa@gmail.com>
2022-08-25 01:54:28 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-/*
-  Add written explanation of your solution here
-*/
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +33,13 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mummy"));
+
+  
 }
 
 module.exports = isPalindrome;
